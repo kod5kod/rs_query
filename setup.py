fd51c4c25efbf0e4bcaf9ba3e110dbdb9557dd15
LeeAdtheorent <lee@adtheorent.com>
2016-01-05 15:41:42 -0500

    fixing error in setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = 'v1.1 Beta'
 setup(
 	name = 'rs_query',
 	version = version,
-	description = 'The "rs_query" module is a simple "psycopg2" wrapper for quering and interacting with Amazon's Redshift.',
+	description = 'The rs_query module is a simple "psycopg2" wrapper for quering and interacting with Amazons Redshift.',
 	author = 'kod5kod Datuman',
 	url = 'https://github.com/kod5kod',
 	include_package_data = True,
