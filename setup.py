a00e00200d34681ca879779a616c6e1c0204b226
LeeAdtheorent <lee@adtheorent.com>
2016-01-05 15:28:18 -0500

    adding setup file and MANIFEST

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+version = 'v1.1 Beta'
+setup(
+	name = 'rs_query',
+	version = version,
+	description = 'The "rs_query" module is a simple "psycopg2" wrapper for quering and interacting with Amazon's Redshift.',
+	author = 'kod5kod Datuman',
+	url = 'https://github.com/kod5kod',
+	include_package_data = True,
+	packages = find_packages(),
+	install_requires = [
+		]
+)
+
+
+
+
+
